a2b2b4808575d3026364467f5cd88d1c21380679
Joe W <whobbes22@gmail.com>
2023-03-22 09:25:24 -0700

    add js for madlibs to show story

---
+++ js/script.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+  // new line below
+  console.log("Script executing!");
+
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    // new line below
+    event.preventDefault();
+    console.log("Submit form successfully reached.");
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput= document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    const nounInput = document.getElementById("nounInput").value;
+
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
+
+    document.querySelector("div#story").removeAttribute("class");
+  };
+};
